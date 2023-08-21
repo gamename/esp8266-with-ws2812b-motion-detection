bc038001f49dcaa34d64ff570626633f79c3683c
gamename <tennis.n.smith@gmail.com>
2023-08-21 18:51:34 -0500

    turned down brightness and only light every other pixel

--- boot.py
+++ boot.py
@@ -1,10 +1,11 @@
 #
 # This is a simple configuration to drive a ws2812b strip of LEDs using a esp2866 microcontroller
 #
+from time import sleep
+
+import network
 from machine import Pin
 from neopixel import NeoPixel
-from time import sleep
-import network
 
 motion = False
 interrupt_pin = None
@@ -29,12 +30,12 @@ PIN_D5 = 14
 SLEEP_MINUTES = 3
 SLEEP_INTERVAL = 60 * SLEEP_MINUTES
 NUM_PIXELS = 55
-LIGHTS_ON = (255, 255, 255)
+LIGHTS_ON = (64, 64, 64)
 LIGHTS_OFF = (0, 0, 0)
 led = Pin(PIN_D3, Pin.OUT)
-np = NeoPixel(led, NUM_PIXELS)
-np.fill(LIGHTS_OFF)
-np.write()
+strip = NeoPixel(led, NUM_PIXELS)
+strip.fill(LIGHTS_OFF)
+strip.write()
 
 pir = Pin(PIN_D5, Pin.IN)
 
@@ -43,10 +44,11 @@ pir.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
 while True:
     if motion:
         # print('Motion detected! Interrupt caused by:', interrupt_pin)
-        np.fill(LIGHTS_ON)
-        np.write()
+        for i in range(0, NUM_PIXELS, 2):
+            strip[i] = LIGHTS_ON
+            strip.write()
         sleep(SLEEP_INTERVAL)
-        np.fill(LIGHTS_OFF)
-        np.write()
+        strip.fill(LIGHTS_OFF)
+        strip.write()
         # print('Motion stopped!')
         motion = False
