c74081dd0618e6bcf40a7646be54c19ff6485ee8
gamename <tennis.n.smith@gmail.com>
2023-09-25 15:34:32 -0500

    update boot

--- boot.py
+++ boot.py
@@ -1,3 +1,10 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+# import esp
+# esp.osdebug(None)
+# uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 
-gc.enable()
+import webrepl
+
+webrepl.start()
+gc.collect()
